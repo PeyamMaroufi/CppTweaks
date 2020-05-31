c7ceba58eea7ec4efd1d9b93512952d837d9edda
Maroufi <maroufi@maroufi.maroufi>
2020-05-31 20:15:35 +0200

    Random values using for loop one liner

--- forloops.cpp
+++ forloops.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <stdlib.h>
 using namespace std;
 int main()
 {
@@ -26,5 +27,12 @@ int main()
     int f_0, f_1, temp;
     cout << "\n\nFibonacci numbers one liner\n";
     for (f_0 = 0, f_1 = 1; f_0 <= 100; temp = f_1, f_1 += f_0, f_0 = temp, cout << f_1 << endl)
+        ;
+
+    /* Print the max random value and creating random numbers */
+    cout << endl;
+    cout << "Max value of random function is : " << RAND_MAX << " Random number generation" << endl;
+    int random_integer;
+    for (int d = 0; d < 15; random_integer = 5 + (rand() % (50 - 5 + 1)), cout << d << ") " << random_integer << endl, d++)
         ;
 }
